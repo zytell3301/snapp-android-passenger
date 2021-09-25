ab06801df4aad999afa4a296c2258c5727157ea5
zytell3301 <zytell3301@gmail.com>
2021-09-26 00:45:04 +0330

    Implement Socket class initilization

--- app/src/main/java/ir/AralStudio/snapp/Socket/Socket.java
+++ app/src/main/java/ir/AralStudio/snapp/Socket/Socket.java
@@ -1,4 +1,23 @@
 package ir.AralStudio.snapp.Socket;
 
+import java.io.IOException;
+
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+import io.nats.client.Options;
+
 public class Socket {
+    protected Options options;
+    protected Connection nats;
+
+    public Socket(String host, int port) throws IOException, InterruptedException {
+        this.options = new Options.Builder().server(host + ":" + port).build();
+        this.nats = Nats.connect(this.options);
+    }
+
+    public Socket(Options options) throws IOException, InterruptedException {
+        this.options = options;
+        this.nats = Nats.connect(this.options);
+    }
+
 }
